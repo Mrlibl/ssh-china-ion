1ba5c67cbd40bdd70521a5f606a03992921281cc
李宝龙 <baolong.li@percent.cn>
2026-05-16 17:01:32 +0800

    Initial commit

---
+++ src/pages/Recruit.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../recruit/Header'
+import JobInfo from '../recruit/Jobinfo'
+
+
+
+function Recruit() {
+  return (
+    <div>
+      <Header/>
+      <main >
+        <div >
+          <JobInfo />
+        </div>
+           
+      </main>
+    </div>   
+    
+  )
+}
+
+export default Recruit
